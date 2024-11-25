7595e60bfd4bd3bebb1f45589fbaf78c9a4da763
Sanskargupta0 <sanskar362002@gmail.com>
2024-11-25 20:40:48 +0530

    Added:- New form Fields added in Sign up form

--- src/utils/SuperTokens.js
+++ src/utils/SuperTokens.js
@@ -1,57 +1,85 @@
 import SuperTokens from "supertokens-auth-react";
-import ThirdParty, {
-  Google
-} from "supertokens-auth-react/recipe/thirdparty";
+import ThirdParty, { Google } from "supertokens-auth-react/recipe/thirdparty";
 import EmailPassword from "supertokens-auth-react/recipe/emailpassword";
 import EmailVerification from "supertokens-auth-react/recipe/emailverification";
 import Session from "supertokens-auth-react/recipe/session";
-import { toast } from 'react-toastify';
-
+import { toast } from "react-toastify";
 
 SuperTokens.init({
-    appInfo: {
-      appName: import.meta.env.VITE_APP_NAME,
-      apiDomain: import.meta.env.VITE_BACKEND_URL,
-      websiteDomain: import.meta.env.VITE_FRONTEND_URL,
-      apiBasePath: "/auth",
-      websiteBasePath: "/auth",
-    },
-    getRedirectionURL: async (context) => {
-      if (context.action === "SUCCESS" && context.newSessionCreated) {
-          let redirectToPath = context.redirectToPath;
-          if (redirectToPath !== undefined) {
-              return redirectToPath;
-          }
-          if (context.createdNewUser) {
-              // user signed up
-              toast.success("User signed up successfully",{
-                position: "top-center",
-              });
-              return "/dashboard"
-          } else {
-              // user signed in
-              toast.success("User signed up successfully",{
-                position: "top-center",
-              });
-              return "/dashboard"
-          }
-      } else if (context.action === "TO_AUTH") {
-          return "/auth";
-      }
-      return undefined;
+  appInfo: {
+    appName: import.meta.env.VITE_APP_NAME,
+    apiDomain: import.meta.env.VITE_BACKEND_URL,
+    websiteDomain: import.meta.env.VITE_FRONTEND_URL,
+    apiBasePath: "/auth",
+    websiteBasePath: "/auth",
   },
-    recipeList: [
-      ThirdParty.init({
-        signInAndUpFeature: {
-          providers: [Google.init()],
+  getRedirectionURL: async (context) => {
+    if (context.action === "SUCCESS" && context.newSessionCreated) {
+      let redirectToPath = context.redirectToPath;
+      if (redirectToPath !== undefined) {
+        return redirectToPath;
+      }
+      if (context.createdNewUser) {
+        // user signed up
+        toast.success("User signed up successfully", {
+          position: "top-center",
+        });
+        return "/dashboard";
+      } else {
+        // user signed in
+        toast.success("User signed up successfully", {
+          position: "top-center",
+        });
+        return "/dashboard";
+      }
+    } else if (context.action === "TO_AUTH") {
+      return "/auth";
+    }
+    return undefined;
+  },
+  recipeList: [
+    ThirdParty.init({
+      signInAndUpFeature: {
+        providers: [Google.init()],
+      },
+    }),
+    EmailVerification.init({
+      mode: "REQUIRED",
+    }),
+    EmailPassword.init({
+      signInAndUpFeature: {
+        signUpForm: {
+          formFields: [
+            {
+              id: "name",
+              label: "Full name",
+              placeholder: "First name and last name",
+              nonOptionalErrorMsg: "Full name is required",
+            },
+            {
+              id: "phone",
+              label: "Phone number",
+              placeholder: "Your phone number",
+              nonOptionalErrorMsg: "Phone number is required",
+              validate: (value) => {
+                if (value.length < 10) {
+                  return "Phone number must be at least 10 digits";
+                }
+                return undefined;
+              },
+            },
+            {
+              id: "country",
+              label: "Your country",
+              placeholder: "Where do you live?",
+              optional: true,
+            },
+          ],
         },
-      }),
-      EmailVerification.init({
-        mode: "REQUIRED",
-      }),
-      EmailPassword.init(),
-      Session.init(),
-    ],
-  });
+      },
+    }),
+    Session.init(),
+  ],
+});
 
 export default SuperTokens;
